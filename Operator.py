77699a1f66f90620fa52e2b4c4b39d4f8dde73d5
aamirali65 <aamiraptech39@gmail.com>
2024-12-15 17:01:34 +0500

    updated

---
+++ Operator.py
@@ -0,0 +1,62 @@
+# Python Arithmetic Operators
+
+#   Operator	Name	        Example
+#     +	        Addition	    x + y	
+#     -	        Subtraction	    x - y	
+#     *	        Multiplication	x * y	
+#     /	        Division	    x / y	
+#     %	        Modulus	        x % y	
+#    **	        Exponentiation	x ** y	
+#    //	        Floor division	x // y
+
+
+# Python Assignment Operators
+
+# Operator	Example	    Same As
+#   =	    x = 5	    x = 5	
+#   +=	    x += 3	    x = x + 3	
+#   -=	    x -= 3	    x = x - 3	
+#   *=	    x *= 3	    x = x * 3	
+#   /=	    x /= 3	    x = x / 3	
+#   %=	    x %= 3	    x = x % 3	
+#   //=	    x //= 3	    x = x // 3	
+#   **=	    x **= 3	    x = x ** 3	
+#   &=	    x &= 3	    x = x & 3	
+#   |=	    x |= 3	    x = x | 3	
+#   ^=	    x ^= 3	    x = x ^ 3	
+#   >>=	    x >>= 3	    x = x >> 3	
+#   <<=	    x <<= 3	    x = x << 3
+
+
+# Python Comparison Operators
+
+#   Operator	Name	                    Example
+#   ==	        Equal	                    x == y	
+#   !=	        Not equal	                x != y	
+#   >	        Greater than	            x > y	
+#   <	        Less than	                x < y	
+#   >=	        Greater than or equal to	x >= y	
+#   <=	        Less than or equal to	    x <= y
+
+
+# Python Logical Operators
+
+#   Operator	Description	                                                    Example
+#   and 	    Returns True if both statements are true	                    x < 5 and  x < 10	
+#   or	        Returns True if one of the statements is true	                x < 5 or x < 4	
+#   not	        Reverse the result, returns False if the result is true	not     (x < 5 and x < 10)
+
+
+# Python Bitwise Operators
+
+#   Operator	Name	Description	                                             Example
+#   & 	        AND	    Sets each bit to 1 if both bits are 1	                 x & y	
+#   |	        OR	    Sets each bit to 1 if one of two bits is 1	             x | y	
+#   ^	        XOR	    Sets each bit to 1 if only one of two bits is 1	         x ^ y	
+#   ~	        NOT	    Inverts all the bits	                                 ~x	
+#   <<	        Zero    fill left shift	Shift left by pushing zeros in from      x << 2
+#                       the right and let the left most bits fall off.
+	             	
+#   >>	        Signed  right shift	Shift right by pushing copies of the         x >> 2
+#                       leftmost bit in from the left,and let the rightmost bits
+#                       fall off
---
+++ Variables.py
@@ -0,0 +1,30 @@
+# simple variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+# Legal variable names
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+# Many Values to Multiple Variables
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+# One Value to Multiple Variables
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+# Case-Sensitive
+a = 4
+A = "Sally"
+#A will not overwrite a
